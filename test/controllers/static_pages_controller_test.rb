d13100a167327d087d6073ec1932df9e07124f93
Sarah Le Cornec <sarahlecornec@Sarahs-MBP.lan>
2015-11-15 19:04:53 +0000

    Finish Home static page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+	def setup
+		@base_title = "Goodreads book reviews"
+	end 
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Goodreads book reviews" 
+    assert_select "title", "Home | #{@base_title}" 
   end
 
 end
